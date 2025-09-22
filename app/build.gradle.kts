23c3e9921bf9f70c22dd9d0a636f4658f6883dab
由风 <youfeng11@outlook.com>
2025-09-22 23:11:13 +0800

    [Chore]修改版本为alpha02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.youfeng.sfs.ctinstaller"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "alpha01"
+        versionCode = 4
+        versionName = "alpha02"
 
         vectorDrawables {
             useSupportLibrary = true
