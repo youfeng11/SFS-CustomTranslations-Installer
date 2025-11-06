d244017938c000ddb5de575112bc3aff0875bfa5
由风 <youfeng11@outlook.com>
2025-11-07 00:09:17 +0800

    [Fix]修复tooltip定位偏移问题

--- app/src/main/kotlin/com/youfeng/sfs/ctinstaller/ui/screen/MainScreen.kt
+++ app/src/main/kotlin/com/youfeng/sfs/ctinstaller/ui/screen/MainScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
@@ -99,10 +100,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.core.net.toUri
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
@@ -608,7 +612,9 @@ private fun LazyItemScope.StatusCard(
                 TextButton(onClick = { openDialog = false }) {
                     Text(stringResource(R.string.cancel))
                 }
-            }
+            },
+            modifier = Modifier.widthIn(max = with(LocalDensity.current) { (LocalWindowInfo.current.containerSize.width * 0.8f).toDp() }),
+            properties = DialogProperties(usePlatformDefaultWidth = false),
         )
     }
 }
