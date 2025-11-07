18eee4b1125364ba630ac0cabab8ab64741539ad
由风 <youfeng11@outlook.com>
2025-11-08 01:32:09 +0800

    [Release]发布0.1.0-beta版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.youfeng.sfs.ctinstaller"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "alpha04"
+        versionCode = 9
+        versionName = "0.1.0-beta"
 
         ndk {
             //noinspection ChromeOsAbiSupport
