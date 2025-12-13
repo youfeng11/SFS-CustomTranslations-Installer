5b5ed6646540631fb02e62051899618cf48971e2
由风 <youfeng11@outlook.com>
2025-12-14 01:14:37 +0800

    [Fix] 修复安装对话框的进度条显示隐藏导致的布局影响

--- app/src/main/kotlin/com/youfeng/sfs/ctinstaller/ui/main/MainScreen.kt
+++ app/src/main/kotlin/com/youfeng/sfs/ctinstaller/ui/main/MainScreen.kt
@@ -38,7 +38,6 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.rememberScrollState
@@ -100,6 +99,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
@@ -1021,8 +1021,7 @@ private fun InstallationProgressContent(
     showBottomDivider: Boolean
 ) {
     Column {
-        if (!uiState.isInstallComplete)
-            LinearProgressIndicator(Modifier.wrapContentWidth())
+        LinearProgressIndicator(Modifier.alpha(if (uiState.isInstallComplete) 0f else 1f))
         Box {
             Box(
                 modifier = Modifier.verticalScroll(scrollState)
